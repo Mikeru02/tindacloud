4c35ff5606dffba681015a8ad630737b6ada8358
Michael Alexis Ponce <michaelalexisponce15@gmail.com>
2026-04-03 15:54:15 +0800

    Added page not found

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
+            
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
