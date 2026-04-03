38ba9fdc12e70aba7fdf0db98b6580468de7835c
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-04-03 14:36:58 +0800

    Added server initial code

--- server/app.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import "dotenv/config";
