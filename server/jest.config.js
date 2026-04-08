4a07f8846dafdcf3eb8ceca7064baa419970c984
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-04-08 08:07:05 +0800

    Modified jest configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,7 +2,6 @@
 export default {
   testEnvironment: "node",
   transform: {}, // Disable standard transformations
-  extensionsToTreatAsEsm: [".js"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1", // Handles .js extensions in imports
   },
