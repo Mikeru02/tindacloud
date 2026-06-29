f35e33ce5e5bbdd39498b227f6f0dfef2ad6e789
Michael Alexis Ponce <mikerusensei0922@gmail.com>
2026-06-29 12:49:29 +0800

    Added cors origin

--- services/nest-api/src/main.ts
+++ services/nest-api/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://tindacloud-merchant.vercel.app'
+    ],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
